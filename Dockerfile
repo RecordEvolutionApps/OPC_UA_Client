e044d0e6121fb85ca7ba14ca6af12818b7901165
Ahmad Bilal <ahmadbilal21444@gmail.com>
2021-01-05 09:46:40 +0000

    single node and object

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,36 @@
 ############################################################
-# Dockerfile to run OPC UA client
+# Dockerfile to run OPC UA server
 # Based on Linux 
 # More details https://docs.docker.com/engine/reference/builder/#format
 ############################################################
 
 # 1. Set a base docker image (see https://hub.docker.com/) and working directory
-FROM mcr.microsoft.com/iotedge/opc-client
+FROM raspbian/stretch
 
 # 2. File Author / Maintainer
 MAINTAINER Record Evolution GmbH
+
+RUN apt-get update && apt-get install -y \
+    python3-pip \
+    git \
+    libffi-dev \
+    libzbar-dev \
+    clang \
+    python3-dev \
+    libssl-dev \
+    build-essential \
+    scons \
+    swig \
+    libxml2-dev \ 
+    libxslt-dev
+
+RUN git clone https://github.com/FreeOpcUa/python-opcua.git
+RUN cd python-opcua && python3 setup.py build && python3 setup.py install
+    
+RUN mkdir /app
+COPY . /app
+WORKDIR /app
+
+CMD python3 -u opc_ua_client.py
+#CMD sleep 10h
+
---
+++ opc_ua_client.py
@@ -0,0 +1,76 @@
+from opcua import ua, uamethod, Client
+from datetime import datetime
+import logging
+import time
+import os
+
+# Get environment variables
+print("********************Get Environment Variables********************")
+SERVER_ADDRESS   = os.environ.get('SERVER_ADDRESS', "opc.tcp://0.0.0.0:4840/freeopcua/server/")
+SERVER_URL_TOPIC = os.environ.get("SERVER_URL_TOPIC", "OPCUA_SERVER_Reswarm")
+ENABLE_ENCRYPTION = os.environ.get('ENABLE_ENCRYPTION', True)
+NODE_READ = os.environ.get("NODE_READ", 1)
+print('User specified values are set')
+
+# Initiate logger
+print("********************Initiating Logger********************")
+logging.basicConfig(level=logging.WARN)
+print("Logging initiated")
+
+# Setup client
+print("********************Set Endpoint of OPC UA Client********************")
+try:
+    client = Client(SERVER_ADDRESS)
+    print("Endpoint for OPC UA client is set to: ", SERVER_ADDRESS)
+except Exception as e:
+    print("Error while setting up endpoint for OPC UA client! ", e)
+    os.system('exit')
+
+print("********************Setting Encryption********************")
+if ENABLE_ENCRYPTION:
+    client.set_security_string("Basic256Sha256,SignAndEncrypt,certificate-example.der,private-key-example.pem")
+    client.secure_channel_timeout = 10000
+    client.session_timeout = 10000
+    print("Client Encryption enabled")
+else:
+    print("Client Encryption disabled")
+
+print("********************Initialize OPC UA Server********************")
+# Connect the client
+try:
+    client.connect()
+except Exception as e:
+    print("Error while connecting the client ", e)
+    os.system('exit')
+
+# Load definition of server specific structures/extension objects
+client.load_type_definitions
+
+# Client has a few methods to get proxy to UA nodes that should always be in address space such as Root or Objects
+root = client.get_root_node()
+print("Root node is: ", root)
+objects = client.get_objects_node()
+print("Objects node is: ", objects)
+
+# Node objects have methods to read and write node attributes as well as browse or populate address space
+print("Children of root are: ", root.get_children())
+
+print("********************Get Namespace********************")
+# gettting our namespace idx
+try:
+    idx_namespace = client.get_namespace_index(SERVER_URL_TOPIC)
+    children_root = root.get_children()
+    print("Registered namespace: ", SERVER_URL_TOPIC)
+except Exception as e:
+    print("Error getting namespace! ", e)
+
+
+try: 
+    while True:
+        for ichild in range(0, NODE_READ):
+            print(children_root[ichild].get_children()[1].get_variables()[0].get_value())
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    client.disconnect()
+
